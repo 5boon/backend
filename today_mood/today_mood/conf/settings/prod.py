bf7ce17c0aa453ac3c8e41e473df689b27085b76
Jay <ckj9014@gmail.com>
2020-03-07 18:55:13 +0900

    [conf] [settings] 코드정리

--- today_mood/today_mood/conf/settings/prod.py
+++ today_mood/today_mood/conf/settings/prod.py
@@ -5,14 +5,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "******"
+SECRET_KEY = '5boon_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'localhost:8000', '127.0.0.1', 'localhost']
 
-OAUTH2_CLIENT_KEY = "******"
+OAUTH2_CLIENT_KEY = '5boon_oauth2_client_key'
 
 AUTH_USER_MODEL = "users.user"
 
