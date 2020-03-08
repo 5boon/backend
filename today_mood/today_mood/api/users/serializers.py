792281ffaad1577f9b792717b556d5420f81fab7
Jay <ckj9014@gmail.com>
2020-03-08 18:39:37 +0900

    [api] [user] serializer 수정

--- today_mood/today_mood/api/users/serializers.py
+++ today_mood/today_mood/api/users/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'date_joined', 'last_login', 'email', 'nickname']
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -25,8 +25,35 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'nickname', 'password', 'is_active')
+        fields = ('username', 'nickname', 'password',)
 
     def create(self, validated_data):
         instance = User.objects.create_user(**validated_data)
         return instance
+
+
+class SnsOauthSerializer(serializers.BaseSerializer):
+    token = serializers.CharField(min_length=1, max_length=100, required=True)
+    sns_type = serializers.ChoiceField(choices=['apple', 'kakao'], required=True)
+
+    class Meta:
+        fields = ['token']
+
+    def to_internal_value(self, data):
+        token = data.get('token')
+        sns_type = data.get('sns_type')
+
+        if not token:
+            raise serializers.ValidationError({
+                'token': 'This field is required.'
+            })
+
+        if not sns_type:
+            raise serializers.ValidationError({
+                'sns_type': 'This field is required.'
+            })
+
+        return {
+            'token': token,
+            'sns_type': sns_type
+        }
