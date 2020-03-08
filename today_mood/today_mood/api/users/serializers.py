2ac958c48b6561277873a90b3fd3d37ea9fc7df4
Jay <ckj9014@gmail.com>
2020-03-08 18:44:58 +0900

    [api] [users] UserSerializer 필드 수정

--- today_mood/today_mood/api/users/serializers.py
+++ today_mood/today_mood/api/users/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'last_login', 'email', 'nickname']
+        fields = ['id', 'date_joined', 'nickname', 'username', 'email']
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
