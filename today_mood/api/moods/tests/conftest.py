0e6f89ba298fe75e87ac59a0eb4b82091b491531
Jay <ckj9014@gmail.com>
2020-03-15 18:01:51 +0900

    [pytest] [api] [moods] 오늘의 기분 Test Case 추가

---
+++ today_mood/api/moods/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from tests.init_data import InitTestData
+
+
+@pytest.fixture(scope='function')
+def user_context():
+    ic = InitTestData()
+
+    # 이름(문자열), 기반 클래스 튜플, 속성과 메서드 딕셔너리
+    ret_data = {
+        'init': ic,
+    }
+    ret = type('context', (), ret_data)
+
+    return ret
