fca5ba83c33b1f3703f7af053f3096833e893d6a
Jay <ckj9014@gmail.com>
2020-03-22 04:38:14 +0900

    [api] [mood-groups] Group detail 호출시, 해당 그룹에 속한 user + today mood 리턴

--- today_mood/api/mood_groups/views.py
+++ today_mood/api/mood_groups/views.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from api.mood_groups.serializers import MoodGroupSerializers, UserMoodGroupSerializers
+from api.moods.serializers import MoodSerializer
+from api.users.serializers import UserSerializer
 from apps.mood_groups.models import MoodGroup, UserMoodGroup
+from apps.moods.models import Mood, UserMood
 
 
 class GroupViewSet(mixins.CreateModelMixin,
@@ -58,3 +61,34 @@ class MyGroupViewSet(mixins.ListModelMixin,
         queryset = self.get_queryset().filter(user=request.user)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        # 그룹에 속한 사람들 리스트
+        user_mood_group = self.get_object()
+        search_group_id = user_mood_group.mood_group.id
+        today_date = timezone.now().date()
+
+        user_mood_group_qs = UserMoodGroup.objects.filter(
+            mood_group_id=search_group_id
+        ).prefetch_related('user')
+
+        user_id_list = list(user_mood_group_qs.values_list('user_id', flat=True))
+        today_user_mood_list = list(UserMood.objects.filter(user_id__in=user_id_list, created__date=today_date))
+
+        user_mood_list = []
+        for user_mood_group in user_mood_group_qs:
+            user_mood = None
+            user_id = user_mood_group.user_id
+            for today_user_mood in today_user_mood_list:
+                if today_user_mood.user_id == user_id:
+                    user_mood = today_user_mood.mood
+
+            data = UserSerializer(instance=user_mood_group.user).data
+            if user_mood:
+                data['mood'] = MoodSerializer(instance=user_mood).data
+            else:
+                data['mood'] = None
+
+            user_mood_list.append(data)
+
+        return Response(user_mood_list)
