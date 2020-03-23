9a65365d568a5639e7994cef9c5ba0efbf3d295a
Jay <ckj9014@gmail.com>
2020-03-23 10:16:29 +0900

    [conf] [settings] github action 전용 세팅 추가

--- today_mood/conf/settings/ci.py
+++ today_mood/conf/settings/ci.py
@@ -1,8 +1,6 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,7 +11,7 @@ SECRET_KEY = '5boon_secret_key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'localhost:8000', '127.0.0.1', 'localhost', '5boon.me']
+ALLOWED_HOSTS = ['*', 'localhost:8000', '127.0.0.1', 'localhost']
 
 OAUTH2_CLIENT_KEY = '5boon_oauth2_client_key'
 
@@ -118,15 +116,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'today_mood.wsgi.application'
 
 #########################################
-#           데이터 베이스 세팅
+#     GitHub Action 데이터 베이스 세팅
 #########################################
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '5boondb',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/conf.d/5boon.cnf',
-        },
+        'NAME': '5boon_ci_db',
+        'USER': 'ci_user',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'ci_user_pw123',
     }
 }
 
@@ -156,19 +154,10 @@ USE_TZ = True
 #########################################
 #           센트리 리포팅
 #########################################
-sentry_sdk.init(
-    dsn="sentry_channel_key",
-    integrations=[DjangoIntegration()],
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
 
 #########################################
 #           슬랙 채널
 #########################################
-SLACK_CHANNEL_JOINED_USER = 'slack_channel_joined_user_key'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
