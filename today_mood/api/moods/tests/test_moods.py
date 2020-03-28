445229a58b82adc4587dabbefe7f74852088634e
Jay <ckj9014@gmail.com>
2020-03-28 18:17:40 +0900

    [pytest] [api] [moods] 필드 추가로 인해 return filed check 변수 수정

--- today_mood/api/moods/tests/test_moods.py
+++ today_mood/api/moods/tests/test_moods.py
@@ -9,7 +9,7 @@ from apps.moods.models import UserMood, Mood
 from apps.users.models import User
 from tests.request_helper import pytest_request
 
-MOOD_FIELDS_LIST = ['id', 'status', 'simple_summary']
+MOOD_FIELDS_LIST = ['id', 'status', 'simple_summary', 'do_show_summary']
 
 
 # Oauth2 인증 Mock 처리 ( TODO: Oauth2.0 도 테스트 될 수 있게 로직 추가해야함 )
@@ -96,4 +96,4 @@ def test_today_mood_list(rf, client, mock_is_authenticated):
                               user=user)
 
     assert response.status_code == status.HTTP_200_OK
-    assert list(response.data.keys()) == MOOD_FIELDS_LIST
+    assert list(response.data[0].keys()) == MOOD_FIELDS_LIST
