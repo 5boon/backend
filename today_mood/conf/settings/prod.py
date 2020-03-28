442e4562838be93234459c9a1b86de2c24046060
Jay <ckj9014@gmail.com>
2020-03-28 16:20:39 +0900

    [conf] [settings] [prod] gmail 관련 config 추가

--- today_mood/conf/settings/prod.py
+++ today_mood/conf/settings/prod.py
@@ -170,6 +170,18 @@ sentry_sdk.init(
 #########################################
 SLACK_CHANNEL_JOINED_USER = 'slack_channel_joined_user_key'
 
+
+#########################################
+#           Gmail SMTP
+#########################################
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = '5boon_email'
+EMAIL_HOST_PASSWORD = '5boon_email_pw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
