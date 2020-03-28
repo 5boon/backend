7cd2f534e5b10be5a83d12b1dcd89145bb61ba86
Jay <ckj9014@gmail.com>
2020-03-28 16:19:34 +0900

    [utils] [slack] SLACK_CHANNEL_JOINED_USER 가 None 이 아난 경우에만 실행 (local과 분리)

--- today_mood/utils/slack.py
+++ today_mood/utils/slack.py
@@ -3,5 +3,6 @@ from slackweb import slackweb
 
 
 def notify_slack(attachments, channel):
-    slack = slackweb.Slack(url=settings.SLACK_CHANNEL_JOINED_USER)
-    slack.notify(channel=channel, attachments=attachments)
+    if settings.SLACK_CHANNEL_JOINED_USER:
+        slack = slackweb.Slack(url=settings.SLACK_CHANNEL_JOINED_USER)
+        slack.notify(channel=channel, attachments=attachments)
