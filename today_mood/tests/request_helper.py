57a933532e11690c35a8143b9565af8091d3b3ea
Jay <ckj9014@gmail.com>
2020-04-02 11:29:29 +0900

    [pytest] [helper] get 파라미터 삭제

--- today_mood/tests/request_helper.py
+++ today_mood/tests/request_helper.py
@@ -5,7 +5,7 @@ from django.urls import resolve
 from rest_framework.test import force_authenticate
 
 
-def pytest_request(rf, method, url, user, get=None, data=None, token=None):
+def pytest_request(rf, method, url, user, data=None, token=None):
     """
         : Test 를 위한 request 생성하는 함수
         - rf 설명: https://pytest-django.readthedocs.io/en/latest/helpers.html#rf-requestfactory
@@ -13,7 +13,7 @@ def pytest_request(rf, method, url, user, get=None, data=None, token=None):
 
     content_type = 'application/json'
     caller = getattr(rf, method)
-    request = caller(path=url, get=get, data=data, content_type=content_type)
+    request = caller(path=url, data=data, content_type=content_type)
 
     if not hasattr(request, 'session'):
         setattr(request, 'session', SessionStore())
@@ -26,13 +26,8 @@ def pytest_request(rf, method, url, user, get=None, data=None, token=None):
     )
     request.user = user
 
-    if isinstance(get, dict):
-        request_url = '{}?{}'.format(url, urlencode(get))
-    else:
-        request_url = url
-
     # 실제 호출하는 url 에 대한 view 정보를 가져옴
-    resolver_match = resolve(request_url)
+    resolver_match = resolve(url)
     test_func, test_args, test_kwargs = resolver_match
     request.resolver_match = resolver_match
     response = test_func(request, *test_args, **test_kwargs)
