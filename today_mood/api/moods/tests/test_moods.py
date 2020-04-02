085f55eb7b3fd88c116b119a087853e68e219cf3
Jay <ckj9014@gmail.com>
2020-04-02 20:16:05 +0900

    [pytest] [api] [moods] 기분 생성 Test Case 수정 ( 속한 그룹들 별로 모든 기분을 생성 )

--- today_mood/api/moods/tests/test_moods.py
+++ today_mood/api/moods/tests/test_moods.py
@@ -4,11 +4,12 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.reverse import reverse
 
+from apps.mood_groups.models import UserMoodGroup, MoodGroup
 from apps.moods.models import UserMood, Mood
 from apps.users.models import User
 from tests.request_helper import pytest_request
 
-MOOD_FIELDS_LIST = ['id', 'status', 'simple_summary', 'do_show_summary']
+MOOD_FIELDS_LIST = ['id', 'status', 'simple_summary']
 
 
 # Oauth2 인증 Mock 처리 ( TODO: Oauth2.0 도 테스트 될 수 있게 로직 추가해야함 )
@@ -25,16 +26,29 @@ def mock_is_authenticated():
     [0, 1, 2, 3, 4]
 )
 def test_today_mood_create(rf, client, mood_status, mock_is_authenticated):
-    data = {
-        "status": mood_status,
-        "simple_summary": "테스트 기분"
-    }
 
     user = User.objects.create(
         username='test_user',
         name='test_name',
         password='test_pw'
     )
+
+    mood_group = MoodGroup.objects.create(
+        title='5boon',
+        summary='5boon 팀원들과의 기분 공유'
+    )
+
+    group = UserMoodGroup.objects.create(
+        user=user,
+        mood_group=mood_group,
+        is_reader=True
+    )
+
+    data = {
+        "status": mood_status,
+        "simple_summary": "테스트 기분",
+        "group_list": [group.id]
+    }
 
     url = reverse(viewname="moods:today_mood")
     response = pytest_request(rf,
