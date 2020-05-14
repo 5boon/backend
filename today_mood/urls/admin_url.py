03399924a8084dcc52a07a323745f2ce9e5780c3
Jay <ckj9014@gmail.com>
2020-05-14 09:27:37 +0900

    [admin] django-3-jet 테마 추가

--- today_mood/urls/admin_url.py
+++ today_mood/urls/admin_url.py
@@ -1,6 +1,9 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', admin.site.urls),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
 ]
