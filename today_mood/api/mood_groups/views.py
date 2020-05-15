f4aa7a2bf986c29f82a5df7328f786d03f19bf65
Jay <ckj9014@gmail.com>
2020-05-15 16:05:21 +0900

    [api] [mood_groups] 페이지네이션 추가

--- today_mood/api/mood_groups/views.py
+++ today_mood/api/mood_groups/views.py
@@ -7,6 +7,7 @@ from rest_framework.viewsets import GenericViewSet
 
 from api.mood_groups.serializers import MoodGroupSerializers, UserMoodGroupSerializers, MoodInvitationSerializers
 from api.moods.serializers import UserMoodSerializers
+from api.pagination import CustomCursorPagination
 from apps.mood_groups.models import MoodGroup, UserMoodGroup, MoodGroupInvitation
 from apps.moods.models import UserMood
 from apps.users.models import User
@@ -133,6 +134,7 @@ class GroupInvitationViewSet(mixins.CreateModelMixin,
     queryset = MoodGroupInvitation.objects.all()
     serializer_class = MoodInvitationSerializers
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    pagination_class = CustomCursorPagination
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -164,10 +166,13 @@ class GroupInvitationViewSet(mixins.CreateModelMixin,
 
     def list(self, request, *args, **kwargs):
         # 자기 자신의 초대장 검색
-        queryset = self.get_queryset().filter(guest=self.request.user)
-        serializer = self.get_serializer(queryset, many=True)
+        self.pagination_class.cursor = self.request.query_params.get('cursor')
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        queryset = self.get_queryset().filter(guest=self.request.user)
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True)
+
+        return self.get_paginated_response(serializer.data)
 
     def update(self, request, *args, **kwargs):
         # 초대 수락
